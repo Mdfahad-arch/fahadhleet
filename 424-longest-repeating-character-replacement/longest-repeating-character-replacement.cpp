005d7c7adf4e3c14aeeb125d4bc527bac342ced8
Mdfahad-arch <fahadmohammadfahad9@gmail.com>
2026-09-15 21:19:48 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (99.47%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        vector<int> freq(26,0);
+        int l = 0;
+        int r = 0;
+        int maxcount  = 0;
+        int maxlength = 0;
+
+        while(r < s.size()){
+            freq[s[r]-'A']++;
+            maxcount = max(maxcount , freq[s[r]-'A']);
+
+            while((r-l+1)-maxcount>k){
+                freq[s[l]-'A']--;
+                l++;
+            }
+            maxlength = max(maxlength,r-l+1);
+            r++;
+        }
+        return maxlength;
+    }
+};
