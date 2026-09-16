4bfbf447d5063ced2223c03d3750f374ef730f07
Mdfahad-arch <fahadmohammadfahad9@gmail.com>
2026-09-16 22:34:04 +0530

    Time: 0 ms (100.00%) | Memory: 32.7 MB (40.18%) - LeetSync

---
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -0,0 +1,31 @@
+class Solution {
+private:
+    // Helper function to count subarrays with sum <= goal
+    int atMost(vector<int>& nums, int goal) {
+        if (goal < 0) return 0;
+
+        int l = 0;
+        int r = 0;
+        int sum = 0;
+        int cnt = 0;
+
+        while (r < nums.size()) {
+            sum += nums[r];
+            
+            while (sum > goal) {
+                sum -= nums[l];
+                l++;
+            }
+            
+            cnt += (r - l + 1);
+            r++;
+        }
+        
+        return cnt;
+    }
+
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        return atMost(nums, goal) - atMost(nums, goal - 1);
+    }
+};
